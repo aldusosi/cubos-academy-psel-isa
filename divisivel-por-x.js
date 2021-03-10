a53ae7f675e153044c6b1d611120db4071310db7
aldusosi <desenho2510@gmail.com>
2021-03-10 11:33:17 -0300

    arq divisivel-por-x adic

---
+++ divisivel-por-x.js
@@ -0,0 +1,10 @@
+function solucao(numero, x) {
+	const num1 = numero
+    const num2 = x
+    
+    if(num1 % x == 0){
+        console.log("true")
+    }else{
+        console.log("false")
+    }
+}
