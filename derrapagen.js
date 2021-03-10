43171f900b14534aae9a8880f450e5234c2049cf
aldusosi <desenho2510@gmail.com>
2021-03-10 11:39:24 -0300

    arq derrapagem adic

---
+++ derrapagen.js
@@ -0,0 +1,9 @@
+function solucao(velocidade, aceleracao) {
+	const n1 = velocidade;
+    const n2 = aceleracao;
+    const num = -n1*(-n1)
+    const den = 2 * n2
+    let soma = num/den* -1;
+    console.log(soma);
+  
+}
