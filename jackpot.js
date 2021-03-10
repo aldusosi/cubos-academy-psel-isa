0166dae0930babbbd9f5f7b0d4f0127a9c88ec6b
aldusosi <desenho2510@gmail.com>
2021-03-10 12:21:16 -0300

    arq jackpot alterado

---
+++ jackpot.js
@@ -0,0 +1,11 @@
+function solucao(resultado1, resultado2, resultado3) {
+	const a = resultado1;
+    const b = resultado2;
+    const c = resultado3;
+    
+    if(a == b && a==c){
+        console.log("true")
+    }else{
+        console.log("false")
+    }
+}
